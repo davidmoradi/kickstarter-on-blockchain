d4f174230a4f38970ab61c40952a4552b9fc06f6
davidmoradi <david@wishpond.com>
2018-04-19 15:12:20 -0700

    Create Campaign Contract + some structure

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.4.17;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvers;
+    }
+
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public contributers;
+    uint public contributersCount;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    constructor(uint minimum, address campaignCreator) public {
+        manager = campaignCreator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value >= minimumContribution);
+
+        contributers[msg.sender] = true;
+        contributersCount++;
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+
+        requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(contributers[msg.sender]);
+        require(!request.approvers[msg.sender]);
+
+        request.approvers[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        // More than 50% of contributers need to aprrove the request
+        require(request.approvalCount > (contributersCount / 2));
+
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}

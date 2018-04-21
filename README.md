1b6c815e6380e3c6eb4c80e02aeb09fcba6e3a2c
davidmoradi <david@wishpond.com>
2018-04-21 13:45:46 -0700

    Changes:   - Create deploy script   - Deployed contract to 0x6aF5836Ec2dDcd479Ed84839c3CF8D32A9217a11   - bring in Ui packages

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Contributors then vote on that request, and will essentially have a say in how
 the capital is being used by the campaign creator. As soon as more than 50% of contributors approve the spending request, the campaign creator can finalize request and the requested value would be sent to the recipient.
 
 Stay tuned...
+
+contract deployed to 0x6aF5836Ec2dDcd479Ed84839c3CF8D32A9217a11 on rinkeby network.
---
+++ ethereum/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compliedFactory = require('./build/CampaignFactory.json');
+
+const providerURL = "https://rinkeby.infura.io/..."; // replace with your Rinkeby test network URL
+const mnemonic = "in crypto we trust ..."; // replace with your 12 word wallet mnemonic.
+
+const provider = new HDWalletProvider(mnemonic, providerURL);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Deploying from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(compliedFactory.interface))
+    .deploy({ data: compliedFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('contract deployed to', result.options.address);
+}
+
+deploy();

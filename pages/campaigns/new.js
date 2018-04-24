0d1d736c8f9af422d5e3ddbdddeb990149c9d25c
davidmoradi <david@wishpond.com>
2018-04-24 00:23:22 -0700

    Basic createCampaign implementation

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import Layout from '../../components/Layout'
+import React, { Component } from 'react';
+import Layout from '../../components/Layout';
+import { Form, Button, Input } from 'semantic-ui-react';
+import factory from '../../ethereum/factory'
+import web3 from '../../ethereum/web3'
 
-class CampaignNew extends React.Component {
+class CampaignNew extends Component {
+  state = {
+    minimumContribution: ''
+  }
+
+  createCampaign = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    await factory.methods.createCampaign(this.state.minimumContribution)
+      .send({
+        from: accounts[0]
+      });
+  }
+
   render() {
     return(
       <Layout>
-        Create a new Campaign
+        <h3>Create a Campaign</h3>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input label='Wei'
+                   labelPosition='right'
+                   value={this.state.minimumContribution}
+                   onChange={event =>
+                     this.setState({ minimumContribution: event.target.value })
+                   } />
+          </Form.Field>
+          <Button primary onClick={this.createCampaign}>Create!</Button>
+        </Form>
       </Layout>
     );
   }

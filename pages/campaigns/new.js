c82c2a2e57e94b4aa02416a0a7a6bdd4c0f8fc48
davidmoradi <david@wishpond.com>
2018-04-24 23:02:36 -0700

    Some error handling when creating a new campaign

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import factory from '../../ethereum/factory'
 import web3 from '../../ethereum/web3'
 
 class CampaignNew extends Component {
   state = {
-    minimumContribution: ''
+    minimumContribution: '',
+    errorMessage: '',
+    loading: false
   }
 
   createCampaign = async () => {
-    const accounts = await web3.eth.getAccounts();
+    this.setState({ loading: true, errorMessage: '' })
 
-    await factory.methods.createCampaign(this.state.minimumContribution)
-      .send({
-        from: accounts[0]
-      });
+    try {
+      const accounts = await web3.eth.getAccounts();
+
+      await factory.methods.createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0]
+        });
+    } catch (error) {
+      console.log(error);
+      this.setState({ errorMessage: error.message})
+    }
+
+    this.setState({ loading: false })
   }
 
   render() {
     return(
       <Layout>
         <h3>Create a Campaign</h3>
-        <Form>
+        <Form error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input label='Wei'
@@ -32,7 +43,10 @@ class CampaignNew extends Component {
                      this.setState({ minimumContribution: event.target.value })
                    } />
           </Form.Field>
-          <Button primary onClick={this.createCampaign}>Create!</Button>
+          <Message error header='Oops!' content={this.state.errorMessage} />
+          <Button primary
+                  onClick={this.createCampaign}
+                  loading={this.state.loading}>Create!</Button>
         </Form>
       </Layout>
     );

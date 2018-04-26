1ddb772b661c179fa1af2d85e6f2fa1c836e30b0
davidmoradi <david@wishpond.com>
2018-04-25 22:54:11 -0700

    More Campaign details

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -2,6 +2,7 @@ import React , { Component } from 'react'
 import Layout from '../../components/Layout'
 import Campaign from '../../ethereum/campaign'
 import { Card } from 'semantic-ui-react';
+import web3 from '../../ethereum/web3'
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -32,7 +33,27 @@ class CampaignShow extends Component {
         meta: 'Address of manager',
         description: 'The manager created this campaign and can create requests to withdraw money',
         style: { overflowWrap: 'break-word' }
-      }
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum Contribution (Wei)',
+        description: `You must contribute at least ${minimumContribution} Wei to become a contributer`,
+      },
+      {
+        header: requestCount,
+        meta: 'Number of Requests',
+        description: 'A Request tries to withdraw money from the contract. Requests must be approved by contributers',
+      },
+      {
+        header: contributersCount,
+        meta: 'Number of Contributers',
+        description: 'Number of people who have already donated to this campaign',
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign Balance (Ether)',
+        description: 'The balance is how much money this campaign has left to spend',
+      },
     ]
 
     return <Card.Group items={items} />;
